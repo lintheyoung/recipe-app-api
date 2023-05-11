425350f8c1cebed58afbfa647918c1228fd785b5
HenryLin <102785622+lintheyoung@users.noreply.github.com>
2023-05-11 12:56:02 +0800

    编辑admin页面

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.User, UserAdmin)
